d73904a6ba8c2926f8f973411416c3cf947c4fb0
liuhx <564030525@qq.com>
2016-06-27 16:24:20 +0800

    SmtpHelper

---
+++ Xin.Basic/Xin.Basic/SmtpHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Xin.Basic
+{
+    public class SmtpHelper
+    {
+        private static string host;
+        private static string from;
+        private static string password;
+
+        static SmtpHelper()
+        {
+            host = System.Configuration.ConfigurationManager.AppSettings["MailHost"];
+            from = System.Configuration.ConfigurationManager.AppSettings["MailFrom"];
+            password = System.Configuration.ConfigurationManager.AppSettings["MailPassword"];
+        }
+
+        public static void Send(string subject, string body, params string[] to)
+        {
+            using (SmtpClient sc = new SmtpClient())
+            {
+                sc.Host = host;
+                sc.Credentials = new NetworkCredential(from, password);
+                sc.EnableSsl = true;
+
+                MailMessage mm = new MailMessage();
+                mm.From = new MailAddress(from);
+                foreach (string toAddress in to)
+                {
+                    mm.To.Add(toAddress);
+                }
+                mm.Subject = subject;
+                mm.Body = body;
+
+                sc.Send(mm);
+                mm.Dispose();
+            }
+        }
+    }
+}
