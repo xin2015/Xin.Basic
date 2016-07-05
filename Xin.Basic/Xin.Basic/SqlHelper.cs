490349bfbe7cc37e83627981c8240373435e4e5e
liuhx <564030525@qq.com>
2016-07-05 12:16:59 +0800

    Default

--- Xin.Basic/Xin.Basic/SqlHelper.cs
+++ Xin.Basic/Xin.Basic/SqlHelper.cs
@@ -10,12 +10,31 @@ namespace Xin.Basic
     /// <summary>
     /// 数据库工具类
     /// </summary>
-    public static class SqlHelper
+    public class SqlHelper
     {
         /// <summary>
-        /// 默认数据库连接
+        /// 数据库连接字符串
         /// </summary>
-        public static readonly string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public string ConnectionString { get; private set; }
+        /// <summary>
+        /// 默认实例
+        /// </summary>
+        public static SqlHelper Default { get; private set; }
+
+        static SqlHelper()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            Default = new SqlHelper(connectionString);
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="connectionString">数据库连接字符串</param>
+        public SqlHelper(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
 
         #region SqlCommand
         public static SqlCommand GetInitSqlCommand(SqlConnection conn, CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
@@ -40,10 +59,10 @@ namespace Xin.Basic
         }
         #endregion
         #region ExecuteNonQuery
-        public static int ExecuteNonQuery(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
+        public int ExecuteNonQuery(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
         {
             int result;
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 conn.Open();
                 SqlCommand cmd = GetInitSqlCommand(conn, cmdType, cmdText, cmdParameters);
@@ -52,33 +71,23 @@ namespace Xin.Basic
             return result;
         }
 
-        public static int ExecuteNonQuery(string connectionString, string cmdText, params SqlParameter[] cmdParameters)
+        public int ExecuteNonQuery(string cmdText, params SqlParameter[] cmdParameters)
         {
             int result;
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 conn.Open();
                 SqlCommand cmd = GetInitSqlCommand(conn, cmdText, cmdParameters);
                 result = cmd.ExecuteNonQuery();
             }
             return result;
-        }
-
-        public static int ExecuteNonQuery(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            return ExecuteNonQuery(connectionString, cmdType, cmdText, cmdParameters);
-        }
-
-        public static int ExecuteNonQuery(string cmdText, params SqlParameter[] cmdParameters)
-        {
-            return ExecuteNonQuery(connectionString, cmdText, cmdParameters);
         }
         #endregion
         #region ExecuteScalar
-        public static object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
+        public object ExecuteScalar(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
         {
             object result;
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 conn.Open();
                 SqlCommand cmd = GetInitSqlCommand(conn, cmdType, cmdText, cmdParameters);
@@ -87,10 +96,10 @@ namespace Xin.Basic
             return result;
         }
 
-        public static object ExecuteScalar(string connectionString, string cmdText, params SqlParameter[] cmdParameters)
+        public object ExecuteScalar(string cmdText, params SqlParameter[] cmdParameters)
         {
             object result;
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 conn.Open();
                 SqlCommand cmd = GetInitSqlCommand(conn, cmdText, cmdParameters);
@@ -98,110 +107,47 @@ namespace Xin.Basic
             }
             return result;
         }
-
-        public static object ExecuteScalar(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
+        #endregion
+        #region ExecuteDataTable
+        public DataTable ExecuteDataTable(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
         {
-            return ExecuteScalar(connectionString, cmdType, cmdText, cmdParameters);
+            DataTable result = new DataTable();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(GetInitSqlCommand(conn, cmdType, cmdText, cmdParameters));
+                adapter.Fill(result);
+            }
+            return result;
         }
 
-        public static object ExecuteScalar(string cmdText, params SqlParameter[] cmdParameters)
+        public DataTable ExecuteDataTable(string cmdText, params SqlParameter[] cmdParameters)
         {
-            return ExecuteScalar(connectionString, cmdText, cmdParameters);
+            DataTable result = new DataTable();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(GetInitSqlCommand(conn, cmdText, cmdParameters));
+                adapter.Fill(result);
+            }
+            return result;
         }
         #endregion
         #region ExecuteList
-        public static List<T> ExecuteList<T>(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters) where T : class, new()
+        public List<T> ExecuteList<T>(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters) where T : class, new()
         {
-            DataTable dt = ExecuteDataTable(connectionString, cmdType, cmdText, cmdParameters);
+            DataTable dt = ExecuteDataTable(cmdType, cmdText, cmdParameters);
             return dt.GetList<T>();
         }
 
-        public static List<T> ExecuteList<T>(string connectionString, string cmdText, params SqlParameter[] cmdParameters) where T : class, new()
+        public List<T> ExecuteList<T>(string cmdText, params SqlParameter[] cmdParameters) where T : class, new()
         {
-            DataTable dt = ExecuteDataTable(connectionString, cmdText, cmdParameters);
+            DataTable dt = ExecuteDataTable(cmdText, cmdParameters);
             return dt.GetList<T>();
-        }
-
-        public static List<T> ExecuteList<T>(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters) where T : class, new()
-        {
-            return ExecuteList<T>(connectionString, cmdType, cmdText, cmdParameters);
-        }
-
-        public static List<T> ExecuteList<T>(string cmdText, params SqlParameter[] cmdParameters) where T : class, new()
-        {
-            return ExecuteList<T>(connectionString, cmdText, cmdParameters);
-        }
-        #endregion
-        #region ExecuteDataTable
-        public static DataTable ExecuteDataTable(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            DataTable result = new DataTable();
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                SqlDataAdapter adapter = new SqlDataAdapter(GetInitSqlCommand(conn, cmdType, cmdText, cmdParameters));
-                adapter.Fill(result);
-            }
-            return result;
-        }
-
-        public static DataTable ExecuteDataTable(string connectionString, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            DataTable result = new DataTable();
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                SqlDataAdapter adapter = new SqlDataAdapter(GetInitSqlCommand(conn, cmdText, cmdParameters));
-                adapter.Fill(result);
-            }
-            return result;
-        }
-
-        public static DataTable ExecuteDataTable(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            return ExecuteDataTable(connectionString, cmdType, cmdText, cmdParameters);
-        }
-
-        public static DataTable ExecuteDataTable(string cmdText, params SqlParameter[] cmdParameters)
-        {
-            return ExecuteDataTable(connectionString, cmdText, cmdParameters);
-        }
-        #endregion
-        #region ExecuteDataSet
-        public static DataSet ExecuteDataSet(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            DataSet result = new DataSet();
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                SqlDataAdapter adapter = new SqlDataAdapter(GetInitSqlCommand(conn, cmdType, cmdText, cmdParameters));
-                adapter.Fill(result);
-            }
-            return result;
-        }
-
-        public static DataSet ExecuteDataSet(string connectionString, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            DataSet result = new DataSet();
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                SqlDataAdapter adapter = new SqlDataAdapter(GetInitSqlCommand(conn, cmdText, cmdParameters));
-                adapter.Fill(result);
-            }
-            return result;
-        }
-
-        public static DataSet ExecuteDataSet(CommandType cmdType, string cmdText, params SqlParameter[] cmdParameters)
-        {
-            return ExecuteDataSet(connectionString, cmdType, cmdText, cmdParameters);
-        }
-
-        public static DataSet ExecuteDataSet(string cmdText, params SqlParameter[] cmdParameters)
-        {
-            return ExecuteDataSet(connectionString, cmdText, cmdParameters);
         }
         #endregion
         #region Insert by SqlBulkCopy
-        public static void Insert(string connetionString, DataTable dt)
+        public void Insert(DataTable dt)
         {
-            using (SqlBulkCopy sbc = new SqlBulkCopy(connetionString))
+            using (SqlBulkCopy sbc = new SqlBulkCopy(ConnectionString))
             {
                 sbc.DestinationTableName = dt.TableName;
                 sbc.BatchSize = 50000;
@@ -211,11 +157,6 @@ namespace Xin.Basic
                 }
                 sbc.WriteToServer(dt);
             }
-        }
-
-        public static void Insert(DataTable dt)
-        {
-            Insert(connectionString, dt);
         }
         #endregion
     }
